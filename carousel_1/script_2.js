fdbf284fe6279750cb6ba3f0a18295369acbb669
Yuriy Vedernikov <yu.piter.gm@gmail.com>
2024-01-15 23:42:12 +0300

    fix: change script_2 file

--- carousel_1/script_2.js
+++ carousel_1/script_2.js
@@ -1,21 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const carouselItems = document.querySelectorAll('.carousel-item');
     let currentSlide = 0;
-    let prevItem = null;
+    let isInitialRender = true;
+    let previousSlide = null
 
     function showSlide(slideIndex) {
         carouselItems.forEach((item, index) => {
-            if (item.classList.contains("active")) return;
 
-            if (index === slideIndex) {
+            if (index === slideIndex && !isInitialRender) {
+                console.log("previousSlide:", previousSlide);
                 item.classList.add('active');
+                carouselItems[previousSlide].classList.add('next');
                 setTimeout(() => {
-
                     item.classList.add('next');
-                }, 1000)
+                    setTimeout(() => {
+                        console.log("previousSlide_2:", previousSlide);
+                        console.log("currentSlide_2:", currentSlide);
+                        carouselItems[previousSlide].classList.remove('next', 'active');
+                        item.classList.remove('next');
+                    }, 3000);
+                }, 1000);
 
-            } else {
-                item.classList.remove('next', 'active');
             }
 
 
@@ -23,6 +28,8 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function nextSlide() {
+        isInitialRender = false;
+        previousSlide = currentSlide;
         currentSlide = (currentSlide + 1) % carouselItems.length;
         console.log("currentSlide:", currentSlide);
         showSlide(currentSlide);
