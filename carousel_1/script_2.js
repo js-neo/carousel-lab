38bb0df99e085b86e5b576aa20b6b5aa15a91be8
Yuriy Vedernikov <yu.piter.gm@gmail.com>
2024-01-16 12:09:59 +0300

    fix: change script_2 file

--- carousel_1/script_2.js
+++ carousel_1/script_2.js
@@ -2,40 +2,40 @@ document.addEventListener('DOMContentLoaded', function () {
     const carouselItems = document.querySelectorAll('.carousel-item');
     let currentSlide = 0;
     let isInitialRender = true;
-    let previousSlide = null
+    let prevSlideIndex = null;
+    let prevSlide = null;
 
     function showSlide(slideIndex) {
         carouselItems.forEach((item, index) => {
 
             if (index === slideIndex && !isInitialRender) {
-                console.log("previousSlide:", previousSlide);
-                item.classList.add('active');
-                carouselItems[previousSlide].classList.add('next');
+                prevSlide = carouselItems[prevSlideIndex];
+                prevSlide.classList.add('next');
                 setTimeout(() => {
-                    item.classList.add('next');
+                    item.classList.add('active');
                     setTimeout(() => {
-                        console.log("previousSlide_2:", previousSlide);
-                        console.log("currentSlide_2:", currentSlide);
-                        carouselItems[previousSlide].classList.remove('next', 'active');
+                        item.classList.add('next');
+                    }, 500)
+
+                    setTimeout(() => {
+                        prevSlide.classList.remove('next', 'active');
                         item.classList.remove('next');
                     }, 3000);
                 }, 1000);
-
             }
-
-
         });
     }
 
-    function nextSlide() {
+    function showNextSlide() {
         isInitialRender = false;
-        previousSlide = currentSlide;
+        prevSlideIndex = currentSlide;
         currentSlide = (currentSlide + 1) % carouselItems.length;
-        console.log("currentSlide:", currentSlide);
         showSlide(currentSlide);
     }
 
-    function prevSlide() {
+    function showPrevSlide() {
+        isInitialRender = false;
+        prevSlideIndex = currentSlide;
         currentSlide = (currentSlide - 1 + carouselItems.length) % carouselItems.length;
         showSlide(currentSlide);
     }
@@ -43,8 +43,8 @@ document.addEventListener('DOMContentLoaded', function () {
     // Добавляем обработчики событий для кнопок переключения слайдов
     const prevButton = document.querySelector('.carousel-control-prev');
     const nextButton = document.querySelector('.carousel-control-next');
-    prevButton.addEventListener('click', prevSlide);
-    nextButton.addEventListener('click', nextSlide);
+    prevButton.addEventListener('click', showPrevSlide);
+    nextButton.addEventListener('click', showNextSlide);
 
     // Показываем первый слайд при загрузке страницы
     showSlide(currentSlide);
